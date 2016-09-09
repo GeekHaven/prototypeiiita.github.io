5064255a955ff68e78f03ad2b4d68473b36f987e
Gautham <thabeatsz@gmail.com>
2016-09-09 13:15:06 +0530

    Update project.js

--- projects/project.js
+++ projects/project.js
@@ -1,3 +1,8 @@
 var projects = {
+	"<project-name>":{
+                      "image":"https://avatars3.githubusercontent.com/u/22071699?v=3&s=200",
+                      "team":"Team IIIT-A Hacks",
+                      "link":"https://github.io/iiitahacks"
+                  },
 	
 }
