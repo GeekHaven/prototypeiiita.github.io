3889c6a94f168cd4c5b3ee4c49aea1b1c5f40d47
Raghav Khandelwal <raghavthecyberdude@gmail.com>
2016-09-11 03:57:53 +0530

    Update project.js

--- projects/project.js
+++ projects/project.js
@@ -1,8 +1,8 @@
 var projects = {
-	"<project-name>":{
+	"Third Eye":{
                       "image":"https://avatars3.githubusercontent.com/u/22071699?v=3&s=200",
-                      "team":"IIIT-A Hacks",
-                      "link":"https://github.com/iiitahacks"
+                      "team":"W.I.C.K.E.D.",
+                      "link":"'https://github.com/RaghavTheCyberdude/ThirdEye"
                   },
 	
 }
