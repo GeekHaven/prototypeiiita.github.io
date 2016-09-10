a30de367658854c146f5dca02090320903905f3f
Diwakar M <diwakar03121997@gmail.com>
2016-09-11 04:47:15 +0530

    Update project.js

--- projects/project.js
+++ projects/project.js
@@ -4,5 +4,11 @@ var projects = {
                       "team":"IIIT-A Hacks",
                       "link":"https://github.com/iiitahacks"
                   },
+         "Gather":{
+                      "image":"https://avatars3.githubusercontent.com/u/22071699?v=3&s=200",
+                      "team":"checkmate",
+                      "link":"https://github.com/magician03/gather"
+                  },
+                 
 	
 }
