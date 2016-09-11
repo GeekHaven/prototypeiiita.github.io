9249647c70f8b3b95f8e75cb363899378b7b24c4
BelieveC <cayadav2009@gmail.com>
2016-09-11 19:54:31 +0530

    Fix

--- projects/project.js
+++ projects/project.js
@@ -3,11 +3,7 @@ var projects = {
                       "image":"https://avatars3.githubusercontent.com/u/22071699?v=3&s=200",
                       "team":"IIIT-A Hacks",
                       "link":"https://github.com/iiitahacks"
-                  },
-     "Ungli": {
-     	"image":"https://raw.githubusercontent.com/BelieveC/Ungli/master/Preview/Ungli.png",
-     	"team":"corrupt_shooters",
-     	"link":"https://github.com/BelieveC/Ungli"
-     }
+                  }
+     
 	
 }
